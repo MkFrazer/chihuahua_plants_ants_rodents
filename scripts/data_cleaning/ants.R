f313600d2af9339e3a12ad073df326f073a790b5
Madeline Frazer <mkfrazer@student.ubc.ca>
2026-03-16 09:49:35 -0700

    cleaned up folder organization (deleted unneccessary folders), worked on ants_clean

--- scripts/data_cleaning/ants.R
+++ scripts/data_cleaning/ants.R
@@ -1,10 +1,12 @@
 ####### Ants raw -> clean
 ###### Filtered to variables YEAR, PLOT, from 1977-2002
 library(dplyr)
+library(here)
+
 
 #read in files to combine
-ants_1 <- read.csv("C:/Users/madel/OneDrive - UBC/UBCO/Courses/Reproducibility/Mini-Project/Data/raw_data/Ants_raw/Portal_ant_colony_19771987.csv")
-ants_2 <- read.csv("C:/Users/madel/OneDrive - UBC/UBCO/Courses/Reproducibility/Mini-Project/Data/raw_data/Ants_raw/Portal_ant_colony_19882002.csv")
+ants_1 <- read.csv(here("Mini-Project", "data", "data_raw", "Ants_raw", "Portal_ant_colony_19771987.csv"))
+ants_2 <- read.csv(here("Mini-Project", "data", "data_raw", "Ants_raw", "Portal_ant_colony_19882002.csv"))
 
 #find column names that were new from 1988-2002 to exclude
 colnames(ants_1)
@@ -21,5 +23,10 @@ names(ants_2.1)[names(ants_2.1) == 'SOLB.pres.1.'] <- 'SOLB'
 #combine both data sets into one data frame
 ants_clean <- full_join(ants_1, ants_2.1)
 
+#limit to years 1983-2002
+ants_clean <- filter(ants_clean, YEAR >= 1983)
+
 #export to .csv
-write.csv(ants_clean, "C:/Users/madel/OneDrive - UBC/UBCO/Courses/Reproducibility/Mini-Project/Data/clean_data/ants_clean.csv", row.names = FALSE)
+output_path <- here("Mini-Project", "data", "data_processed")
+write.csv(ants_clean, output_path, row.names = FALSE)
+
