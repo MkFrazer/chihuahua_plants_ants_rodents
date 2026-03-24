ba2140fdec6482d5b534ef8ac0977879b07fd197
Madeline Frazer <mkfrazer@student.ubc.ca>
2026-03-24 11:25:31 -0700

    working on model - glmmTMB and variance

--- scripts/analysis/model_comparison.R
+++ scripts/analysis/model_comparison.R
@@ -1,4 +1,5 @@
-#this script is for model comparison using AICc to determine which factors best predict plant abundance
+#this script is for model comparison using AICc to determine which factors best predict plant abundance,
+#and to correct for potential autocorrelation
 
 ####AGGREGATION
 
@@ -71,6 +72,16 @@ model0 <- glm.nb(total_abundance ~ All_ants + Annual_precip + (1|Year/Plot),
                  data = data,
                  link = log,
                  na.action = "na.fail")
+
+###heteroskedasticity
+plot(model0$fitted.values, model0$residuals)
+#compare residuals against the factors in the model that aren't autocorrelation
+plot(data$All_ants, model0$residuals)
+plot(data$Annual_precip, model0$residuals)
+
+
+###Autocorrelation
+
 #check for temporal autocorrelation
 plot(data$Year, model0$residuals)
 all_resid <- residuals(model0, type = "pearson")
@@ -78,24 +89,6 @@ acf(all_resid)
 
 #check for temporal autocorrelation within each plot
 library(DHARMa)
-#simulated residuals
-# sim_res <- simulateResiduals(model0)
-# norm_resid <- sim_res$scaledResiduals
-# 
-# data$norm_resid <- norm_resid
-# 
-# unique_plots <- unique(data$Plot)
-# 
-# par(mfrow = c(4, 6))
-# for (p in unique_plots) {
-#   plot_data <- data[data$Plot == p, ]
-#   plot_data <- data[order(plot_data$Year),]
-#   
-#   if (nrow(plot_data) > 5) {
-#     acf(plot_data$norm_resid)
-#   }
-}
-#actual residuals
 
 data$all_resid <- all_resid
 
@@ -111,14 +104,73 @@ for (p in unique_plots) {
 
 sim <- simulateResiduals(fittedModel = model0)
 plot(sim)
+par(mfrow = c(1,1))
+plotQQunif(sim) 
+plotResiduals(sim)
+###acf shows no temporal autocorrelation per plot 
+
+#plot residuals versus annual precip
+plotResiduals(sim, form = data$Year)
+plotResiduals(sim, form = data$Annual_precip)
+
+plot(data$Annual_precip, data$total_abundance)
+###increasing precip associated with increasing total abundance
+
+#add variance structure for precip using model comparison
+#fixed, power, exponential, but I have to change to glmmTMB to do that
+library(glmmTMB)
+
+#scale the variables to try and fix issues with glmmTMB
+data$Annual_precip_s <- scale(data$Annual_precip)
+
+model_tmb<- glmmTMB(total_abundance ~ All_ants + Annual_precip_s + (1|Year/Plot), 
+                data = data,
+                family = nbinom2(link = "log"),
+                na.action = "na.fail",
+                REML = FALSE)
+model_fixed <-glmmTMB(total_abundance ~ All_ants + Annual_precip_s + (1|Year/Plot),
+                      data = data,
+                      family = nbinom2(link="log"),
+                      dispformula = ~Annual_precip_s,
+                      na.action = "na.fail", 
+                      REML = FALSE)
+model_power <- glmmTMB(total_abundance ~ All_ants + Annual_precip_s + (1|Year/Plot),
+                       data = data,
+                       family = nbinom2(link="log"),
+                       dispformula = ~log(Annual_precip),
+                       na.action = "na.fail", 
+                       REML = FALSE)
+model_exp <- glmmTMB(total_abundance ~ All_ants + Annual_precip_s + (1|Year/Plot),
+                     data = data,
+                     family = nbinom2(link="log"),
+                     dispformula = ~(Annual_precip)^2,
+                     na.action = "na.fail", 
+                     REML = FALSE)
+
+AIC(model_tmb, model_fixed, model_power)
+###fixed variance is the better model
+
+par(mfrow = c(1,1))
+par(mar = c(5.1, 4.1, 4.1, 2.1))
+plot(fitted(model_fixed), residuals(model_fixed, type = "pearson"))
+
+fixed_sim <- simulateResiduals(model_fixed)
+plot(fixed_sim)
+###still correlation, likely spatial
+
+#check for spatial autocorrelation within each year
+plot(data$Plot, residuals(model_fixed))
+
+plotResiduals(fixed_sim, form = data$Plot)
+plotResiduals(fixed_sim, form = data$Year)
+plotResiduals(fixed_sim, form = data$Annual_precip)
 
 
-###acf shows no temporal autocorrelation
 
-# spatial autocorrelation
-# plot as random effect
-# precip as factor
-# random slope of annual precip?
-#   temporal autocorrealtion, but look plot by plot ove ryear
-# spatial autocorrelation: look at spatial correlation in each year
-# (1|year/plot)
+par(mfrow = c(4, 6))
+for (p in unique_plots) {
+  plot_data <- data[data$Plot == p, ]
+  plot_data <- data[order(plot_data$Year),]
+  
+  acf(plot_data$all_resid, main = paste("Plot ", p))
+}
